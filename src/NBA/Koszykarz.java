c7a3c6d875481547293650b595e76cdb809e9a96
geciu <getekf@gmail.com>
2018-11-08 15:38:20 +0100

    Update main file

--- src/NBA/Koszykarz.java
+++ src/NBA/Koszykarz.java
@@ -12,23 +12,50 @@ public class Koszykarz {
 
     final static double salary = 94;
 
-    Koszykarz(String imieNazwisko, double zarobki, int numery[]){
+    Koszykarz(String imieNazwisko, int zarobki, int numery[]){
         this.imieNazwisko = imieNazwisko;
         this.zarobki = zarobki;
         this.numery = numery;
     }
 
-    Koszykarz(String imieNazwisko, double zarobki){
+    Koszykarz(String imieNazwisko, int zarobki){
         this.imieNazwisko = imieNazwisko;
         this.zarobki = zarobki;
     }
 
     public void tyleZarabiam(){
         double procentSalary = (zarobki/salary)*100;
-        if(procentSalary < 40) System.out.println(imieNazwisko + " zarabia " + zarobki + " dolarow, co stanowi " + procentSalary + "% salary klubu. Jest on dobrym graczem");
-        else if(procentSalary >= 40 && procentSalary < 60) System.out.println(imieNazwisko + " zarabia " + zarobki + " dolarow, co stanowi " + procentSalary + "% salary klubu. Jest on bardzo dobrym graczem");
-        else if(procentSalary >= 60 && procentSalary < 80) System.out.println(imieNazwisko + " zarabia " + zarobki + " dolarow, co stanowi " + procentSalary + "% salary klubu. Jest on All-Stare'em");
-        else System.out.println(imieNazwisko + " zarabia " + zarobki + " dolarow, co stanowi " + procentSalary + "% salary klubu. Jest on przepłaconym graczem");
+        double wartosc = procentSalary;
+
+        if(wartosc >= 0 && wartosc < 40) {wartosc=0;}
+        else if(wartosc >= 40 && wartosc < 60) {wartosc=1;}
+        else if(wartosc >= 60 && wartosc < 80) {wartosc=2;}
+        else {wartosc=3;};
         System.out.println();
+
+        switch((int)wartosc){
+            case 0: System.out.println(imieNazwisko + " zarabia " + zarobki + "mln $, co stanowi " + procentSalary + "% salary klubu. Jest on dobrym graczem");
+            break;
+            case 1: System.out.println(imieNazwisko + " zarabia " + zarobki + "mln $, co stanowi " + procentSalary + "% salary klubu. Jest on bardzo dobrym graczem");
+            break;
+            case 2: System.out.println(imieNazwisko + " zarabia " + zarobki + "mln $, co stanowi " + procentSalary + "% salary klubu. Jest on All-Stare'em");
+            break;
+            case 3: System.out.println(imieNazwisko + " zarabia " + zarobki + "mln $, co stanowi " + procentSalary + "% salary klubu. Jest on przepłaconym graczem");
+            break;
+            default: System.out.println("Wystąpił problem. Spróbuj jeszcze raz.");
+        }
     }
+
+    public void zainteresowanie(Liga.Kluby klub){
+        int ile = 0;
+
+        for (int i =0; i <  numery.length; i++){
+            ile++;
+        }
+
+        System.out.println("Zawodnikiem " + imieNazwisko + ", który nosił " + ile + " numerów na koszulce, interesuje się klub " + klub);
+    }
+
+
+
 }
--- src/NBA/Liga.java
+++ src/NBA/Liga.java
@@ -5,8 +5,11 @@ import java.awt.*;
 
 public class Liga {
 
+    public enum Kluby {Celtics, lakers, Heat, Warriors}
+
     public static void main (String args[]){
-        Koszykarz Kobe = new Koszykarz("Kobe", 90);
+        Koszykarz Kobe = new Koszykarz("Kobe", 90, new int[]{8, 24});
         Kobe.tyleZarabiam();
+        Kobe.zainteresowanie(Kluby.lakers);
     }
 }
